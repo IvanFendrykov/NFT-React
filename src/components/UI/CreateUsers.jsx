ab44c0f37718d6584997501d64f5d6f516acb092
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2025-10-27 22:36:15 +0100

    Update Create section

---
+++ src/components/UI/CreateUsers.jsx
@@ -0,0 +1,28 @@
+import users from "../../creators.json";
+function CreateUsers() {
+  return (
+    <>
+      <ul className="CreatorsList">
+        {users.map((u, i) => {
+          return (
+            <li className="CreatorsItem" key={u.name}>
+              <div className="Position">
+                <div className="PositionAbs">{i + 1}</div>
+                <img src={u.imageurl} alt={u.name} />
+              </div>
+              <div className="CreatorsName">
+                <h5>{u.name}</h5>
+                <div>
+                  <span>Total Sales:</span>
+                  <span>{u.eth} ETH</span>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default CreateUsers;
